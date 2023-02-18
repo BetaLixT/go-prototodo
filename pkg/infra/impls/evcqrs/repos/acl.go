b1639eb22c38bdaed83bbe8a76a896fef628a276
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2023-02-18 16:46:54 +0400

    acl implementation

--- pkg/infra/impls/evcqrs/repos/acl.go
+++ pkg/infra/impls/evcqrs/repos/acl.go
@@ -2,6 +2,8 @@ package repos
 
 import (
 	"context"
+	"database/sql"
+	"math"
 	"prototodo/pkg/domain/base/acl"
 	"prototodo/pkg/domain/base/logger"
 	domcom "prototodo/pkg/domain/common"
@@ -13,6 +15,7 @@ import (
 	"time"
 
 	"github.com/go-redis/redis/v8"
+	"github.com/lib/pq"
 	"github.com/patrickmn/go-cache"
 	"go.uber.org/zap"
 )
@@ -24,6 +27,8 @@ type ACLRepository struct {
 	lgrf    logger.IFactory
 	keySffx string
 }
+
+var _ acl.IRepository = (*ACLRepository)(nil)
 
 func NewACLRepository(
 	base *BaseDataRepository,
@@ -82,12 +87,99 @@ func (r *ACLRepository) CreateACLEntry(
 }
 
 func (r *ACLRepository) DeleteACLEntry(
-	ctx context.Context,
+	c context.Context,
 	stream string,
 	streamID string,
 	userType string,
 	userID string,
 ) error {
+	lgr := r.lgrf.Create(c)
+
+	ctx, ok := c.(cntxt.IContext)
+	if !ok {
+		lgr.Error("unexpected context type")
+		return common.NewFailedToAssertContextTypeError()
+	}
+
+	dbtx, err := r.getDBTx(ctx)
+	if err != nil {
+		lgr.Error("failed to get db transaction", zap.Error(err))
+		return err
+	}
+
+	var entry entities.ACL
+	err = dbtx.Get(
+		ctx,
+		&entry,
+		DeleteACLEntryQuery,
+		userType,
+		userID,
+		stream,
+		streamID,
+	)
+	if err != nil {
+		lgr.Error("failed to delete entry")
+		return err
+	}
+	err = r.rctx.ZRem(
+		ctx,
+		r.keySffx+userType+":"+userID,
+		generateACLSetMember(entry.Stream, entry.StreamID, entry.Permissions),
+	).Err()
+	if err != nil {
+		lgr.Error("failed to delete cache entry", zap.Error(err))
+		return err
+	}
+	return nil
+}
+
+func (r *ACLRepository) DeleteACLEntries(
+	c context.Context,
+	stream string,
+	streamID string,
+) error {
+	lgr := r.lgrf.Create(c)
+
+	ctx, ok := c.(cntxt.IContext)
+	if !ok {
+		lgr.Error("unexpected context type")
+		return common.NewFailedToAssertContextTypeError()
+	}
+
+	dbtx, err := r.getDBTx(ctx)
+	if err != nil {
+		lgr.Error("failed to get db transaction", zap.Error(err))
+		return err
+	}
+
+	var entry []entities.ACL
+	err = dbtx.Select(
+		ctx,
+		&entry,
+		DeleteACLEntriesQuery,
+		stream,
+		streamID,
+	)
+	if err != nil {
+		lgr.Error("failed to delete entries", zap.Error(err))
+		return err
+	}
+
+	rpipe := r.rctx.Pipeline()
+	for idx := range entry {
+		rpipe.ZRem(
+			ctx,
+			r.keySffx+entry[idx].UserType+":"+entry[idx].UserId,
+			generateACLSetMember(stream, streamID, entry[idx].Permissions),
+		)
+	}
+
+	_, err = rpipe.Exec(ctx)
+	if err != nil {
+		lgr.Error("failed to delete cache entries")
+		return err
+	}
+	return nil
 }
 
 func (r *ACLRepository) CanRead(
@@ -97,13 +189,26 @@ func (r *ACLRepository) CanRead(
 	userType string,
 	userID string,
 ) error {
-	perm, err := r.getEntry(
-		ctx,
-		stream,
-		streamIDs,
-		userType,
-		userID,
-	)
+	perm := 0
+	var err error
+	if len(streamIDs) == 1 {
+		perm, err = r.getEntry(
+			ctx,
+			stream,
+			streamIDs[0],
+			userType,
+			userID,
+		)
+	} else {
+		perm, err = r.getEntries(
+			ctx,
+			stream,
+			streamIDs,
+			userType,
+			userID,
+		)
+	}
+
 	if err != nil {
 		return err
 	}
@@ -120,13 +225,26 @@ func (r *ACLRepository) CanWrite(
 	userType string,
 	userID string,
 ) error {
-	perm, err := r.getEntry(
-		ctx,
-		stream,
-		streamIDs,
-		userType,
-		userID,
-	)
+	perm := 0
+	var err error
+	if len(streamIDs) == 1 {
+		perm, err = r.getEntry(
+			ctx,
+			stream,
+			streamIDs[0],
+			userType,
+			userID,
+		)
+	} else {
+		perm, err = r.getEntries(
+			ctx,
+			stream,
+			streamIDs,
+			userType,
+			userID,
+		)
+	}
+
 	if err != nil {
 		return err
 	}
@@ -136,6 +254,7 @@ func (r *ACLRepository) CanWrite(
 	return domcom.NewUserACLCheckFailedError()
 }
 
+// getEntries gets acl entries, optimized to get a multiple records
 func (r *ACLRepository) getEntries(
 	ctx context.Context,
 	stream string,
@@ -169,7 +288,7 @@ func (r *ACLRepository) getEntries(
 			continue
 		}
 		if split[0] == stream {
-			per, err := strconv.Atoi(split[1])
+			per, err := strconv.Atoi(split[2])
 			if err != nil {
 				lgr.Warn(
 					"unabled to parse acl entry's permission field to int",
@@ -182,15 +301,18 @@ func (r *ACLRepository) getEntries(
 
 	perm := 0b11
 	rpipe := r.rctx.Pipeline()
+	defer func() {
+		// Keeping the entire LRU cache alive if it's being used, if nmt used for
+		// more than two hours for this stream and this user, the cache will be
+		// deleted
+		rpipe.ExpireAt(
+			ctx,
+			rkey,
+			time.Now().Add(2*time.Hour),
+		)
+		rpipe.Exec(ctx)
+	}()
 
-	// Keeping the entire LRU cache alive if it's being used, if not used for
-	// more than two hours for this stream and this user, the cache will be
-	// deleted
-	rpipe.ExpireAt(
-		ctx,
-		rkey,
-		time.Now().Add(2*time.Hour),
-	)
 	nfidx := 0
 	notFound := make([]string, len(streamIDs))
 	for idx := range streamIDs {
@@ -210,16 +332,52 @@ func (r *ACLRepository) getEntries(
 			nfidx++
 		}
 		if perm == 0 {
-			rpipe.Exec(ctx)
 			return perm, nil
 		}
 	}
+
+	if nfidx == 0 {
+		return perm, nil
+	}
+
+	// Finding uncached ACL entries
+	var dbEntries []entities.ACL
+	err = r.dbctx.Select(
+		ctx,
+		&dbEntries,
+		SelectACLEntriesQuery,
+		userType,
+		userID,
+		stream,
+		pq.StringArray(notFound[:nfidx]),
+	)
+	if err != nil {
+		lgr.Error("failure while quering database", zap.Error(err))
+		return 0, err
+	}
+
+	if len(dbEntries) != nfidx {
+		lgr.Warn("some acl entries were not present")
+		return 0, nil
+	}
+
+	mems := make([]*redis.Z, nfidx)
+	for idx := range dbEntries {
+		perm = perm & dbEntries[idx].Permissions
+		mems[idx] = &redis.Z{
+			Member: generateACLSetMember(
+				stream,
+				dbEntries[idx].StreamID,
+				dbEntries[idx].Permissions,
+			),
+			Score: -math.MaxFloat64,
+		}
+	}
+	rpipe.ZAdd(ctx, rkey, mems...)
+	return perm, nil
 }
 
-func generateACLSetMember(stream string, id string, perm int) string {
-	return stream + ":" + id + ":" + strconv.Itoa(perm)
-}
-
+// getEntry gets acl entry, optimized to get a single record
 func (r *ACLRepository) getEntry(
 	ctx context.Context,
 	stream string,
@@ -228,10 +386,11 @@ func (r *ACLRepository) getEntry(
 	userID string,
 ) (int, error) {
 	lgr := r.lgrf.Create(ctx)
+	rkey := r.keySffx + userType + ":" + userID
 
 	entries, err := r.rctx.ZRange(
 		ctx,
-		r.keySffx+userType+":"+userID,
+		rkey,
 		0,
 		100,
 	).Result()
@@ -240,27 +399,85 @@ func (r *ACLRepository) getEntry(
 		lgr.Error("failed to range through set", zap.Error(err))
 	}
 
-	parsedRes := map[string]int{}
+	perm := 0
+	rpipe := r.rctx.Pipeline()
+	defer func() {
+		// Keeping the entire LRU cache alive if it's being used, if nmt used for
+		// more than two hours for this stream and this user, the cache will be
+		// deleted
+		rpipe.ExpireAt(
+			ctx,
+			rkey,
+			time.Now().Add(2*time.Hour),
+		)
+		rpipe.Exec(ctx)
+	}()
+
 	for idx := range entries {
-		split := strings.Split(entries[idx], ":")
-		if len(split) != 3 {
-			lgr.Warn(
-				"invalid split while parsing redis cached acl entry",
-				zap.String("entry", entries[idx]),
-			)
-			continue
-		}
-		if split[0] == stream {
-			per, err := strconv.Atoi(split[1])
+		if strings.HasPrefix(entries[idx], stream+":"+streamID+":") {
+			split := strings.Split(entries[idx], ":")
+			if len(split) != 3 {
+				lgr.Warn(
+					"invalid split while parsing redis cached acl entry",
+					zap.String("entry", entries[idx]),
+				)
+				break
+			}
+			perm, err = strconv.Atoi(split[2])
 			if err != nil {
 				lgr.Warn(
 					"unabled to parse acl entry's permission field to int",
 					zap.String("entry", entries[idx]),
 				)
+				break
 			}
-			parsedRes[split[1]] = per
+			rpipe.ZIncrBy(
+				ctx,
+				rkey,
+				1,
+				generateACLSetMember(stream, streamID, perm),
+			)
+			break
 		}
 	}
+
+	if perm != 0 {
+		return perm, nil
+	}
+
+	var entry entities.ACL
+	err = r.dbctx.Get(
+		ctx,
+		&entry,
+		SelectACLEntryQuery,
+		userType,
+		userID,
+		stream,
+		streamID,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			lgr.Warn("no acl entry found")
+			return 0, nil
+		}
+		lgr.Error("failed to fetch acl entry")
+		return 0, err
+	}
+
+	rpipe.ZAdd(ctx, rkey, &redis.Z{
+		Member: generateACLSetMember(
+			stream,
+			streamID,
+			entry.Permissions,
+		),
+		Score: -math.MaxFloat64,
+	})
+
+	return entry.Permissions, nil
+}
+
+func generateACLSetMember(stream string, id string, perm int) string {
+	return stream + ":" + id + ":" + strconv.Itoa(perm)
 }
 
 // - Queries
@@ -275,5 +492,27 @@ const (
 	) VALUES (
 		$1, $2, $3, $4, $5
 	) RETURNING *
+	`
+
+	SelectACLEntriesQuery = `
+  SELECT * FROM acls
+  WHERE user_type = $1 AND user_id = $2 AND stream = $3 AND stream_ids = ANY($4)
+	`
+
+	SelectACLEntryQuery = `
+  SELECT * FROM acls
+  WHERE user_type = $1 AND user_id = $2 AND stream = $3 AND stream_ids = $4
+	`
+
+	DeleteACLEntryQuery = `
+  DELETE acls
+  WHERE user_type = $1 AND user_id = $2 AND stream = $3 AND stream_ids = $4
+  RETURNING *
+	`
+
+	DeleteACLEntriesQuery = `
+  DELETE acls
+  WHERE stream = $1 AND stream_ids = $2
+  RETURNING *
 	`
 )

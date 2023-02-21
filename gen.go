ef20dd10c40f0dd87768b3956b687ee9ea23cfad
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2023-02-21 19:24:23 +0400

    updated swagger gen

--- gen.go
+++ gen.go
@@ -9,6 +9,7 @@ package main
 //go:generate protoc --go_out=paths=source_relative:./pkg/domain/ contracts/models.proto
 //go:generate hndlrgen
 //go:generate protoc -I=pkg/infra/impls/evcqrs/entities --go_out=. pkg/infra/impls/evcqrs/entities/data.proto
+//go:generate cp pkg/app/server/contracts/service.http.json pkg/app/server/static/swagger/swagger.json
 
 //go:generate easyjson -all --lower_camel_case ./pkg/domain/contracts/models.pb.go
 
